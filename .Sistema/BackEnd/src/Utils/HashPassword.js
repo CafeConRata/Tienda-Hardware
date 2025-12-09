9c665b26b10a813fdd3ed9d2d58eed860c93dbb3
CafeConRata <leancaffe@gmail.com>
2025-12-09 08:31:15 -0300

    actualizacion del backend

--- .Sistema/BackEnd/src/Utils/HashPassword.js
+++ .Sistema/BackEnd/src/Utils/HashPassword.js
@@ -1,18 +1,18 @@
-// ---> Sirve para encriptar las contraseñas de los usuarios
+
 const Encriptar= require('bcrypt')
-// ---> Numero de saltos para encriptar la contraseña
+
 const Salto=10;
 
-// ----> Funcion para encriptar la contraseña
+
 const EncriptarPassword= async (Password)=>{
-    // ---> Generar el salto y encriptar la contraseña
+    
     const Seguridad= await Encriptar.genSalt(Salto)
-    // ---> Retornar la contraseña encriptada
+    
     return Encriptar.hash(Password,Seguridad)
 }
-//  ----> Funcion para desencriptar la contraseña
+
 const DesincriptarPassword= async (Password,hash)=>{
-    // ---> Retornar si la contraseña coincide con el hash
+    
     return Encriptar.compare(Password,hash)
 }
 
